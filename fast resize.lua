6d57a276df97845fbfaf07793e49d91eab051e02
PeaPattern <72760536+PeaPattern@users.noreply.github.com>
2023-03-17 06:26:30 -0400

    smaller
    fast reize

--- fast resize.lua
+++ fast resize.lua
@@ -1,30 +1,22 @@
-local Player = game:GetService("Players").LocalPlayer
-local Character = Player.Character
+local Character = game:GetService("Players").LocalPlayer.Character
 local Humanoid = Character:FindFirstChildOfClass("Humanoid")
 
 local function Clear()
     repeat
-        local found = Character:FindFirstChild("OriginalSize", true)
+        found = Character:FindFirstChild("OriginalSize", true)
         if found then found:Destroy() end
-    until not Character:FindFirstChild("OriginalSize", true)
+    until not found
     repeat
-        local found = Character:FindFirstChild("OriginalPosition", true)
+        found = Character:FindFirstChild("OriginalPosition", true)
         if found then found:Destroy() end
-    until not Character:FindFirstChild("OriginalPosition", true)
+    until not found
     repeat
-        local found = Character:FindFirstChild("AvatarPartScaleType", true)
+        found = Character:FindFirstChild("AvatarPartScaleType", true)
         if found then found:Destroy() end
-    until not Character:FindFirstChild("AvatarPartScaleType", true)
+    until not found
 end
 
-local Order = {
-    "BodyProportionScale",
-    "BodyHeightScale",
-    "BodyWidthScale",
-    "BodyDepthScale",
-    "HeadScale"
-}
-
+local Order={"BodyProportionScale","BodyHeightScale","BodyWidthScale","BodyDepthScale","HeadScale"}
 for _ = 1, 5 do
     Clear()
     Humanoid:FindFirstChild(Order[_]):Destroy()
